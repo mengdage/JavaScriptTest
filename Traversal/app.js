27543aefea2b5b0211429f506a166f90af5dba16
meng <wulalameng@gmail.com>
2017-06-27 22:35:25 -0400

    node traversal

--- Traversal/app.js
+++ Traversal/app.js
@@ -1,24 +1,26 @@
 (function(global){
   'use strict';
 
-  function optOnAllChildrenNode(root, callback) {
-    var child;
-    if(root.hasChildNodes()) {
-      for(child = root.firstChild; child; child=child.nextSibling) {
-        optOnAllChildrenNode(child, callback);
-      }
-    }
-    callback(root);
-  }
+  // function optOnAllChildrenNode(root, callback) {
+  //   var child;
+  //   if(root.hasChildNodes()) {
+  //     for(child = root.firstChild; child; child=child.nextSibling) {
+  //       optOnAllChildrenNode(child, callback);
+  //     }
+  //   }
+  //   callback(root);
+  // }
+  //
+  //
+  // function printNode(node) {
+  //   if(node.nodeType === 1) {
+  //     console.log(node.nodeName+(node.className? '.'+node.className:'')+ ': ' + node.textContent);
+  //
+  //   }
+  // }
+  // var body = document.body;
+  // optOnAllChildrenNode(body, printNode);
 
-
-  function printNode(node) {
-    if(node.nodeType === 1) {
-      console.log(node.nodeName+(node.className? '.'+node.className:'')+ ': ' + node.textContent);
-
-    }
-  }
-  var body = document.body;
-  optOnAllChildrenNode(body, printNode);
-
+  var header = document.getElementsByTagName('h1');
+  console.log(header.draggable);
 })(self);
