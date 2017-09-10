d4991cbc513661eaa6013c4fa6fa8a23f3d93599
meng <wulalameng@gmail.com>
2017-09-09 23:23:03 -0400

    add html-webpack-plugin

--- hello-react/webpack.config.js
+++ hello-react/webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginconfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 console.log(path.resolve(__dirname,'dist'));
 module.exports = {
   entry: './client/index.js',
@@ -10,5 +17,7 @@ module.exports = {
     loaders: [
       {test: /\.js$|\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
     ]
-  }
+  },
+
+  plugins: [HtmlWebpackPluginconfig]
 }
